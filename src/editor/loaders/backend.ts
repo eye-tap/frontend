90a85004cac4682ce9c179560ec5780f227c13df
Janis Hutz <development@janishutz.com>
2026-02-15 14:05:19 +0100

    [Editor] Loading from backend prepared

--- src/editor/loaders/backend.ts
+++ src/editor/loaders/backend.ts
@@ -1,13 +1,71 @@
-import annotations from '@/ts/annotations';
+import {
+    type Ref,
+    ref
+} from 'vue';
+import {
+    annotations,
+    boundingBoxes,
+    fixations
+} from '../data';
+import type {
+    AnnotationSessionDto
+} from '@/types/dtos/AnnotationSessionDto';
+import type {
+    Renderer
+} from '../types/renderer';
+import annotationManager from '@/ts/annotations';
 import {
     useActiveFileStore
 } from '@/ts/stores/activeFileStore';
 
-const loadEditorDataFromBackend = async () => {
+export const sessionData: Ref<AnnotationSessionDto> = ref( {} );
+
+export const loadEditorDataFromBackend = async ( renderer: Renderer ) => {
     const session = useActiveFileStore();
-    const data = await annotations.getSessionById( session.sessionIds[ session.sessionIdx ]! );
-    // TODO: Load image into image object (do NOT modify onload!!)
-    // TODO: Get annotations into EditorAnnotation[] format
-    // TODO: Get bounding boxes into EditorCharacterBoundingBoxes[] format
-    // TODO: Get fixations into EditorFixation[] format
+
+    sessionData.value = await annotationManager.getSessionById( session.sessionIds[ session.sessionIdx ]! );
+    // Load image
+    const img = sessionData.value.readingSession!.textDto!.backgroundImage!;
+
+    renderer.textImage.src = img;
+
+    // Load annotations
+    const fixIds: number[] = [];
+    const annotationLoad = sessionData.value.annotations!;
+
+    annotations.value = [];
+    annotationLoad.forEach( annotation => {
+        annotations.value.push( {
+            'fixationId': annotation.fixation!.id!,
+            'boxId': annotation.characterBoundingBox!.id!
+        } );
+    } );
+    // Load fixations
+    const fix = sessionData.value.readingSession!.fixations!;
+
+    fixations.value = [];
+    fix.forEach( f => {
+        if ( !fixIds.includes( f.id! ) ) {
+            fixations.value.push( {
+                'x': f.x!,
+                'y': f.y!,
+                'id': f.id!,
+                'assigned': 'unassigned'
+            } );
+        }
+    } );
+
+    // Load bounding boxes
+    const bb = sessionData.value.readingSession!.textDto!.characterBoundingBoxes!;
+
+    boundingBoxes.value = [];
+    bb.forEach( b => {
+        boundingBoxes.value.push( {
+            ...b,
+            'centerX': ( b.xMin! + b.xMax! ) / 2,
+            'centerY': ( b.yMin! + b.yMax! ) / 2,
+            'nearbyPoints': [],
+            'highlightClass': 'none'
+        } );
+    } );
 };
