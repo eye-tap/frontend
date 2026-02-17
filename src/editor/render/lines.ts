40b99a905bf4c93d84326047acd5bcac55d375ff
bickerda <146366238+bickerda@users.noreply.github.com>
2026-02-17 10:48:38 +0000

    [Frontend] Line draw all - formatted by Eslint [skip ci]

--- src/editor/render/lines.ts
+++ src/editor/render/lines.ts
@@ -39,7 +39,7 @@ export const linesRenderer = ( linesCanvas: Ref<HTMLCanvasElement | null> ) => {
         // Render
         if ( linesDisplay.value === 'all' ) {
             annotations.value.forEach( l => {
-                    drawLine( l );
+                drawLine( l );
             } );
         } else if ( linesDisplay.value === 'previous' ) {
             if ( selectedFixation.value >= 0 )
