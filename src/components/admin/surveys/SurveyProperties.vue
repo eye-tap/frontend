3eecf0aeb2c2a271e843d7fe22a7f460977d3538
Entjic <52130476+Entjic@users.noreply.github.com>
2026-02-25 11:55:33 +0000

    [Frontend] Fix export - formatted by Eslint [skip ci]

--- src/components/admin/surveys/SurveyProperties.vue
+++ src/components/admin/surveys/SurveyProperties.vue
@@ -1,164 +1,164 @@
 <script setup lang="ts">
-import {
-  deleteSurvey, exportSurvey
-} from '@/ts/surveys';
-import {
-  useNotification
-} from '@kyvg/vue3-notification';
-import {
-  useRoute
-} from 'vue-router';
-import {
-  useStatusStore
-} from '@/ts/stores/status';
-import {
-  useSurveyStore
-} from '@/ts/stores/admin';
-import {
-  watch
-} from 'vue';
+    import {
+        deleteSurvey, exportSurvey
+    } from '@/ts/surveys';
+    import {
+        useNotification
+    } from '@kyvg/vue3-notification';
+    import {
+        useRoute
+    } from 'vue-router';
+    import {
+        useStatusStore
+    } from '@/ts/stores/status';
+    import {
+        useSurveyStore
+    } from '@/ts/stores/admin';
+    import {
+        watch
+    } from 'vue';
 
-// TODO: Download magic links from server to display (if this is supported)
-// --> Likely won't be due to bcrypt or the like being used for passwords
+    // TODO: Download magic links from server to display (if this is supported)
+    // --> Likely won't be due to bcrypt or the like being used for passwords
 
-const route = useRoute();
-const surveyStore = useSurveyStore();
-const maxSurveyLength = 50;
-const notifications = useNotification();
-const status = useStatusStore();
+    const route = useRoute();
+    const surveyStore = useSurveyStore();
+    const maxSurveyLength = 50;
+    const notifications = useNotification();
+    const status = useStatusStore();
 
-const removeSurvey = () => {
-  deleteSurvey(surveyStore.getSelectedSurveyID!).then((success: boolean) => {
-    if (success) {
-      notifications.notify({
-        'text': 'Survey was deleted successfuly',
-        'type': 'success',
-        'title': 'Survey Deleted'
-      });
-      window.location.reload();
-    } else {
-      notifications.notify({
-        'text': 'Survey could not be deleted',
-        'type': 'error',
-        'title': 'Error: Survey Deletion'
-      });
-    }
-  });
-};
+    const removeSurvey = () => {
+        deleteSurvey( surveyStore.getSelectedSurveyID! ).then( ( success: boolean ) => {
+            if ( success ) {
+                notifications.notify( {
+                    'text': 'Survey was deleted successfuly',
+                    'type': 'success',
+                    'title': 'Survey Deleted'
+                } );
+                window.location.reload();
+            } else {
+                notifications.notify( {
+                    'text': 'Survey could not be deleted',
+                    'type': 'error',
+                    'title': 'Error: Survey Deletion'
+                } );
+            }
+        } );
+    };
 
-const exportThisSurvey = () => {
-  exportSurvey(surveyStore.getSelectedSurveyID!).then((success: boolean) => {
-    if (success)
-      notifications.notify({
-        'text': 'Survey exported successfuly',
-        'type': 'success',
-        'title': 'Survey Export'
-      });
-    else
-      notifications.notify({
-        'text': 'Survey could not be exported',
-        'type': 'error',
-        'title': 'Error: Survey Export'
-      });
-  });
-};
+    const exportThisSurvey = () => {
+        exportSurvey( surveyStore.getSelectedSurveyID! ).then( ( success: boolean ) => {
+            if ( success )
+                notifications.notify( {
+                    'text': 'Survey exported successfuly',
+                    'type': 'success',
+                    'title': 'Survey Export'
+                } );
+            else
+                notifications.notify( {
+                    'text': 'Survey could not be exported',
+                    'type': 'error',
+                    'title': 'Error: Survey Export'
+                } );
+        } );
+    };
 
-const truncate = (text: string, limit: number) => {
-  if (text.length < limit) return text;
-  else return text.slice(0, limit - 3) + '...';
-};
+    const truncate = ( text: string, limit: number ) => {
+        if ( text.length < limit ) return text;
+        else return text.slice( 0, limit - 3 ) + '...';
+    };
 
-const useTestData = () => {
-  let links = [];
+    const useTestData = () => {
+        let links = [];
 
-  for (let i = 0; i < 20; i++)
-    links.push('link' + String(i));
+        for ( let i = 0; i < 20; i++ )
+            links.push( 'link' + String( i ) );
 
-  links.push('linkWhichIsVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLong');
-  surveyStore.setLinks(links);
+        links.push( 'linkWhichIsVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryVeryLong' );
+        surveyStore.setLinks( links );
 
-  notifications.notify({
-    'text': 'Populated link list using testing data for frontend dev.',
-    'type': 'warn',
-    'title': 'Loaded Testing Data'
-  });
-};
+        notifications.notify( {
+            'text': 'Populated link list using testing data for frontend dev.',
+            'type': 'warn',
+            'title': 'Loaded Testing Data'
+        } );
+    };
 
-/**
- *  Selected survey is derived via route (contains unique survey id)
- */
-watch(
-    () => route.params.id,
-    newId => {
-      console.log(newId);
+    /**
+     *  Selected survey is derived via route (contains unique survey id)
+     */
+    watch(
+        () => route.params.id,
+        newId => {
+            console.log( newId );
 
-      if (newId === undefined) surveyStore.setSurveyIndex(-1);
-      else surveyStore.setSurveyIndexById(parseInt(newId as string));
-    }
-);
+            if ( newId === undefined ) surveyStore.setSurveyIndex( -1 );
+            else surveyStore.setSurveyIndexById( parseInt( newId as string ) );
+        }
+    );
 
-if (status.devMode) useTestData();
+    if ( status.devMode ) useTestData();
 </script>
 
 <template>
-  <div class="survey-properties">
-    <div class="top-bar">
-      <h2
-          v-if="surveyStore.selectedSurveyIndex >= 0"
-          class="title"
-      >
-        {{ truncate(surveyStore.getSelectedSurvey?.title!, maxSurveyLength) }}
-      </h2>
-      <h2
-          v-else
-          class="title"
-      >
-        Survey Properties
-      </h2>
-      <div
-          v-if="surveyStore.selectedSurveyIndex >= 0"
-          class="bar-buttons"
-      >
+    <div class="survey-properties">
+        <div class="top-bar">
+            <h2
+                v-if="surveyStore.selectedSurveyIndex >= 0"
+                class="title"
+            >
+                {{ truncate(surveyStore.getSelectedSurvey?.title!, maxSurveyLength) }}
+            </h2>
+            <h2
+                v-else
+                class="title"
+            >
+                Survey Properties
+            </h2>
+            <div
+                v-if="surveyStore.selectedSurveyIndex >= 0"
+                class="bar-buttons"
+            >
                 <span>
                     <i
                         class="fa-solid fa-trash fa-lg trash-icon"
                         @click="removeSurvey"
                     ></i>
                 </span>
-        <span>
+                <span>
                     <i
                         class="fa-solid fa-download fa-lg dl-icon"
                         @click="exportThisSurvey"
                     ></i>
                 </span>
-      </div>
+            </div>
+        </div>
+        <div
+            v-if="surveyStore.selectedSurveyIndex >= 0"
+            class="content"
+        >
+            <p class="subtitle">
+                DESCRIPTION
+            </p>
+            <div class="description-wrapper">
+                <p>
+                    {{ surveyStore.getSelectedSurvey?.description! }}
+                </p>
+            </div>
+            <p class="subtitle">
+                PROPERTIES
+            </p>
+            <div class="properties-wrapper">
+                <p>Users: <strong> {{ surveyStore.getSelectedSurvey?.userIds?.length }} </strong></p>
+            </div>
+        </div>
+        <div
+            v-else
+            class="placeholder"
+        >
+            <p>Select a Survey to view properties</p>
+        </div>
     </div>
-    <div
-        v-if="surveyStore.selectedSurveyIndex >= 0"
-        class="content"
-    >
-      <p class="subtitle">
-        DESCRIPTION
-      </p>
-      <div class="description-wrapper">
-        <p>
-          {{ surveyStore.getSelectedSurvey?.description! }}
-        </p>
-      </div>
-      <p class="subtitle">
-        PROPERTIES
-      </p>
-      <div class="properties-wrapper">
-        <p>Users: <strong> {{ surveyStore.getSelectedSurvey?.userIds?.length }} </strong></p>
-      </div>
-    </div>
-    <div
-        v-else
-        class="placeholder"
-    >
-      <p>Select a Survey to view properties</p>
-    </div>
-  </div>
 </template>
 
 <style scoped lang="scss">
--- src/ts/surveys/index.ts
+++ src/ts/surveys/index.ts
@@ -15,11 +15,11 @@ import request from '../util/request';
 
 
 export const listReadingSessions = async (): Promise<ShallowReadingSessionDto[]> => {
-    return await (await request.getRequest('/reading-session')).json() as ShallowReadingSessionDto[];
+    return await ( await request.getRequest( '/reading-session' ) ).json() as ShallowReadingSessionDto[];
 };
 
 export const listSurveys = async (): Promise<SurveyDto[]> => {
-    return await (await request.getRequest('/survey')).json() as SurveyDto[];
+    return await ( await request.getRequest( '/survey' ) ).json() as SurveyDto[];
 };
 
 export const createSurvey = async (
@@ -34,11 +34,11 @@ export const createSurvey = async (
         'description': description,
         'readingSessionIds': readingSessionIds
     };
-    const surveyUsers = (await (await request.post('/survey', config)).json() as SurveyCreatedDto).users!;
+    const surveyUsers = ( await ( await request.post( '/survey', config ) ).json() as SurveyCreatedDto ).users!;
     const links: string[] = [];
 
-    for (const username in surveyUsers) {
-        links.push(magicLinks.generate(username, surveyUsers[username] as string));
+    for ( const username in surveyUsers ) {
+        links.push( magicLinks.generate( username, surveyUsers[username] as string ) );
     }
 
     return links;
@@ -47,27 +47,26 @@ export const createSurvey = async (
 export const deleteSurvey = async (
     id: number
 ): Promise<boolean> => {
-    return (await request.deleteRequest('/survey/' + id)).status === 200;
+    return ( await request.deleteRequest( '/survey/' + id ) ).status === 200;
 };
 
-export const exportSurvey = async (id: number): Promise<boolean> => {
+export const exportSurvey = async ( id: number ): Promise<boolean> => {
     try {
-        const response = await request.getRequest('/export/survey/' + id);
-
+        const response = await request.getRequest( '/export/survey/' + id );
         const blob = await response.blob();
-        const link = URL.createObjectURL(blob);
+        const link = URL.createObjectURL( blob );
+        const a = document.createElement( 'a' );
 
-        const a = document.createElement('a');
         a.href = link;
         a.download = 'survey' + id + '.csv';
         a.click();
 
-        URL.revokeObjectURL(link);
+        URL.revokeObjectURL( link );
 
         return true;
+    } catch ( e ) {
+        console.log( e );
 
-    } catch (e) {
-        console.log(e)
         return false;
     }
 };
