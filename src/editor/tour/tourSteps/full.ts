68c85916d799b5ba3f7718b988e308361930a376
bickerda <146366238+bickerda@users.noreply.github.com>
2026-06-21 11:42:44 +0200

    [Editor] slight tour adjustments

--- src/editor/tour/tourSteps/full.ts
+++ src/editor/tour/tourSteps/full.ts
@@ -8,13 +8,13 @@ import {
 export const stepsFull: ITourStep[] = [
     {
         'target': '#tour-editor',
-        'content': '<h3>Editor</h3><p>This is the Editor. Pressing a letter on your keyboard will assign the selected point to the closest appearance of that letter. If you press shift and the letter, it will assign the <b>second</b> closest letter. Alternatively, you may click the desired letter, or drag the point to the correct letter.</p>',
+        'content': '<h3>Editor</h3><p>This is the Editor. Pressing a letter on your keyboard will assign the selected point to the closest occurrence of that letter. If you press shift and the letter, it will assign the <b>second</b> closest letter. Alternatively, you may click the desired letter, or drag the point to the correct letter.</p>',
         'highlight': true,
         'backdrop': true
     },
     {
         'target': '#tour-history',
-        'content': '<h3>History</h3><p>Undo (Ctrl + Z) and redo (Ctrl + Y) your actions. <br> Press (Ctrl + S) to save. You can mark a fixation as invalid (Shift + Backspace) by clicking the cross symbol</p>',
+        'content': '<h3>History</h3><p>Undo (Ctrl + Z) and redo (Ctrl + Y) your actions. <br> Press (Ctrl + S) to save. You can mark a fixation as invalid by clicking the cross symbol (or Shift + Backspace).</p>',
         'highlight': true,
         'backdrop': true
     },
