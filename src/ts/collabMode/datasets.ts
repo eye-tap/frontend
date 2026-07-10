f656b543ef59a398e847ded24f7c584143373537
Janis Hutz <development@janishutz.com>
2026-07-10 09:20:48 +0200

    feat: integrate dataset retrieval

--- src/ts/collabMode/datasets.ts
+++ src/ts/collabMode/datasets.ts
@@ -3,30 +3,55 @@ import {
     onMounted,
     ref
 } from 'vue';
+import type {
+    ShallowSurveyDto
+} from '@/types/dtos/ShallowSurveyDto';
 import request from '../util/request';
 import {
     useAnnotationSessionStore
 } from '../stores/annotationSessionStore';
+import {
+    useNotification
+} from '@kyvg/vue3-notification';
 
 export const useDatasets = () => {
-    const datasets: Ref<object> = ref( {} );
+    const datasets: Ref<ShallowSurveyDto[]> = ref( [] );
     const store = useAnnotationSessionStore();
+    const selected: Ref<ShallowSurveyDto> = ref( {
+        'id': 1
+    } );
+    const loading = ref( false );
+    const notifications = useNotification();
 
-    const loadDatasets = async () => {
-        datasets.value = JSON.parse( await request.get( '/annotation/session' ) ) as object[];
+    const load = async () => {
+        loading.value = true;
+
+        try {
+            datasets.value = JSON.parse( await request.get( '/survey/public' ) ) as ShallowSurveyDto[];
+        } catch ( error ) {
+            console.error( error );
+            notifications.notify( {
+                'text': 'Failed to retrieve datasets from the backend',
+                'type': 'error',
+                'title': 'Dataset listing'
+            } );
+        }
+
+        loading.value = false;
     };
 
-    const selectDataset = ( id: number ) => {
-        store.surveyId = id;
+    const select = ( survey: ShallowSurveyDto ) => {
+        selected.value = survey;
+        store.surveyId = survey.id!;
     };
 
     onMounted( () => {
-        loadDatasets();
+        load();
     } );
 
     return {
-        loadDatasets,
+        load,
         datasets,
-        selectDataset
+        select
     };
 };
